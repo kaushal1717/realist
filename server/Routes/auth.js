c675e2df36d180b8749b340b7b71f685674a72ff
kaushal1717 <kpadaliya1717@gmail.com>
2023-11-16 15:28:49 +0530

    task done: setting up aws ses for sending emails and jwt for authentication

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -3,6 +3,8 @@ import * as authController from '../controllers/AuthController.js';
 
 const router = express.Router();
 
-router.get("/",authController.welcome_back);
+router.get("/",authController.welcome);
+router.post("/pre-register", authController.preRegister);
+router.post("/register", authController.register);
 
 export default router;
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,5 +1,62 @@
+import * as config from "../config.js"
+import jwt from "jsonwebtoken";
+
 export const welcome = (req,res) => {
     res.json({
         data:"Hello world from auth controller"
     });
 };
+
+export const preRegister = async (req, res) => {
+    try {
+        const {email, password} = req.body
+        const token = jwt.sign({email, password}, config.JWT_SECRET, {expiresIn: "1h"})
+        config.AWSSES.sendEmail({
+            Source: config.EMAIL_FROM,
+            Destination: {
+                ToAddresses: ["kaushalpadaliya.aids21@scet.ac.in"],
+            },
+            Message: {
+                Body:{
+                    Html:{
+                        Charset: "UTF-8",
+                        Data: `
+                            <html>
+                                <h1> Welcome to realist </h1>
+                                <p> You are one step closer to register yourself. Please click the link given below to activate your account</p>
+                                <a href="${config.CLIENT_URL}/auth/accountactivate/${token}">Activate your account</a>
+                            </html>
+                        `
+                    }
+                },
+                Subject: {
+                    Charset: "UTF-8",
+                    Data: "Welcome to realist"
+                }
+            }
+        },(err,data)=>{
+            if (err) {
+                console.log(err);
+                return res.json({status: "not sent"});
+            } else {
+                console.log(data);
+                return res.json({status: "sent"});
+            }
+        }
+    );  
+    } catch (err) {
+        console.log(err);
+        return res.json({error: 'Something went wrong try again'})
+    }
+}
+
+export const register = async (req, res) => {
+    try {
+        console.log(req.body);
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            error: "Something went wrong try again !!"
+        })
+    }
+}
